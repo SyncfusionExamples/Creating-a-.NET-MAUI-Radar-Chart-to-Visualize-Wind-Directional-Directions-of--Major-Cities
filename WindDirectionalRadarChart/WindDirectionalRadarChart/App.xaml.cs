e7e10be0512be80d8bbe8e6371618d5b3c6d29ac
Surya Karan Raja <113962276+SuryaKaran2143@users.noreply.github.com>
2024-07-31 16:58:31 +0530

    Update App.xaml.cs

--- WindDirectionalRadarChart/WindDirectionalRadarChart/App.xaml.cs
+++ WindDirectionalRadarChart/WindDirectionalRadarChart/App.xaml.cs
@@ -4,7 +4,6 @@
     {
         public App()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cWGhBYVFyWmFZfVpgdl9HZFZRQGYuP1ZhSXxXdkJhUH9Zcn1URGVZVkM=");
             InitializeComponent();
 
             MainPage = new AppShell();
