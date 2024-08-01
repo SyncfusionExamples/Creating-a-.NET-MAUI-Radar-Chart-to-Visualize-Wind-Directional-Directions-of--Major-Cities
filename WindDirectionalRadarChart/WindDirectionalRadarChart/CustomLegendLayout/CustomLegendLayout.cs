7ba631c955895ce45a71323449cef0a00e00354a
SuryaKaran2143 <Suryakaran.r@syncfusion.com>
2024-08-01 17:11:55 +0530

    I commit the changes

---
+++ WindDirectionalRadarChart/WindDirectionalRadarChart/CustomLegendLayout/CustomLegendLayout.cs
@@ -0,0 +1,12 @@
+﻿using Syncfusion.Maui.Charts;
+
+namespace WindDirectionalRadarChart
+{
+    public class LegendExt : ChartLegend
+    {
+        protected override double GetMaximumSizeCoefficient()
+        {
+            return 1;
+        }
+    }
+}
---
+++ WindDirectionalRadarChart/WindDirectionalRadarChart/Model/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace WindDirectionalRadarChart
+{
+    public class ClimateData
+    {
+        public string? Direction { get; set; }
+        public string? DirectionShortForm{ get; set; }
+        public double Brasilia{ get; set; }
+        public double Tokyo { get; set; }
+        public double London { get; set; }
+        public double Nairobi { get; set; }
+        public Brush? SolidColorBrush {  get; set; }
+    }
+}
